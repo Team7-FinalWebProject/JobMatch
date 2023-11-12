47cfbb8b0f881e3a41442f9ee07cb1dcd2248646
stiliamm <redemption.records@icloud.com>
2023-11-12 13:40:23 +0200

    added Company and Prof models (NOT FINISHED)

--- Api/data/models.py
+++ Api/data/models.py
@@ -1,1 +1,19 @@
-pass
+from pydantic import BaseModel
+
+
+class Company(BaseModel):
+    pass
+
+    @classmethod
+    def from_query_result(cls, id):
+        return cls(
+            id=id)
+
+
+class Professional(BaseModel):
+    pass
+
+    @classmethod
+    def from_query_result(cls, id):
+        return cls(
+            id=id)
