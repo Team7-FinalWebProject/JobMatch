858ca58b312e2f1458d559b2327cc588672110e7
DonkoTonev <donkotonev04@gmail.com>
2023-11-12 20:18:14 +0200

    Created login enpoint(Very Basic)

--- Api/routers/login.py
+++ Api/routers/login.py
@@ -1,1 +1,16 @@
-pass
+from fastapi import APIRouter, Response, Query
+from services import login_service
+
+
+login_router = APIRouter(prefix='/login')
+
+
+@login_router.post('/professional', tags=["Login"])
+def login(username: str = Query(), password: str = Query()):
+    user = login_service.try_login(username, password)
+
+    if user:
+        token = login_service.create_token(user)
+        return {'token': token}
+    else:
+        return Response(status_code=404, content='Invalid login data')
--- Api/services/login_service.py
+++ Api/services/login_service.py
@@ -1,1 +1,16 @@
-pass
+from data.database import read_query, insert_query, update_query, update_queries_transaction
+from data.models import Professional, Company
+
+
+def try_login(username: str, password: str) -> Professional | None:
+    user = find_by_username(username)
+
+    return user if user and user.password == password else None
+
+
+def find_by_username(nickname: str) -> Professional | None:
+    data = read_query(
+        'SELECT * FROM professionals WHERE username = %s',
+        (nickname,))
+
+    return next((Professional.from_query_result(*row) for row in data), None)
