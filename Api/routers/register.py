3fe6605fd63626cdc7e5895e767ce2eff1002510
DonkoTonev <donkotonev04@gmail.com>
2023-11-12 21:16:44 +0200

    Just added the functuonality to register as company

--- Api/routers/register.py
+++ Api/routers/register.py
@@ -6,14 +6,27 @@ register_router = APIRouter(prefix='/register')
 
 
 @register_router.post('/professionals', tags=["Signup"])
-def register(username: str = Query(),
+def register_as_professional(username: str = Query(),
              first_name: str = Query(),
              last_name: str = Query(),
              password: str = Query(),
              ):
   
-    if register_service.check_username_exist(username):
+    if register_service.check_professional_exist(username):
         return Response(status_code=400, content=f'Username is already taken!')
     else:
-        user = register_service.create_user(username, first_name, last_name, password)
+        user = register_service.create_professional(username, first_name, last_name, password)
+        return user
+
+
+@register_router.post('/companies', tags=["Signup"])
+def register_as_company(username: str = Query(),
+             company_name: str = Query(),
+             password: str = Query(),
+             ):
+  
+    if register_service.check_company_exist(username):
+        return Response(status_code=400, content=f'Username is already taken!')
+    else:
+        user = register_service.create_company(username, company_name, password)
         return user
--- Api/services/register_service.py
+++ Api/services/register_service.py
@@ -2,7 +2,7 @@ from data.database import read_query, insert_query, update_query, update_queries
 from data.models import Professional, Company
 
 
-def create_user(username: str, first_name:str, last_name:str, password: str) -> Professional | None:
+def create_professional(username: str, first_name:str, last_name:str, password: str) -> Professional | None:
         generated_id = insert_query(
             'INSERT INTO professionals(username, first_name, last_name, password) VALUES (%s, %s, %s, %s)',
             (username, first_name, last_name, password))
@@ -10,11 +10,29 @@ def create_user(username: str, first_name:str, last_name:str, password: str) -> 
         return Professional(id=generated_id, username=username, first_name=first_name, last_name=last_name, password="")
 
 
-def check_username_exist(nickname:str) -> bool:
+def create_company(username: str, company_name:str, password: str) -> Company | None:
+        generated_id = insert_query(
+            'INSERT INTO companies(username, company_name, password) VALUES (%s, %s, %s)',
+            (username, company_name, password))
+
+        return Company(id=generated_id, username=username, company_name=company_name, password="")
+
+
+def check_professional_exist(nickname:str) -> bool:
 
     data = read_query(
         "SELECT username FROM professionals WHERE username = %s",
         (nickname,)
+    )
+
+    return bool(data)
+
+
+def check_company_exist(company_name:str) -> bool:
+
+    data = read_query(
+        "SELECT username FROM companies WHERE username = %s",
+        (company_name,)
     )
 
     return bool(data)
