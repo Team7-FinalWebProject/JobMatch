8eb47687684d9600e4c48b022f3dcea287cc2605
DonkoTonev <donkotonev04@gmail.com>
2023-11-12 19:43:24 +0200

    Register enpoint (very basic)

--- Api/routers/register.py
+++ Api/routers/register.py
@@ -1,1 +1,19 @@
-pass
+from fastapi import APIRouter, Response, Query
+from services import register_service
+from common import auth
+
+register_router = APIRouter(prefix='/register')
+
+
+@register_router.post('/professionals', tags=["Signup"])
+def register(username: str = Query(),
+             first_name: str = Query(),
+             last_name: str = Query(),
+             password: str = Query(),
+             ):
+  
+    if register_service.check_username_exist(username):
+        return Response(status_code=400, content=f'Username is already taken!')
+    else:
+        user = register_service.create_user(username, first_name, last_name, password)
+        return user
--- Api/services/register_service.py
+++ Api/services/register_service.py
@@ -1,1 +1,20 @@
-pass
+from data.database import read_query, insert_query, update_query, update_queries_transaction
+from data.models import Professional, Company
+
+
+def create_user(username: str, first_name:str, last_name:str, password: str) -> Professional | None:
+        generated_id = insert_query(
+            'INSERT INTO professionals(username, first_name, last_name, password) VALUES (?,?,?,?)',
+            (username, first_name, last_name, password))
+
+        return Professional(id=generated_id, username=username, first_name=first_name, last_name=last_name, password="")
+
+
+def check_username_exist(nickname:str) -> bool:
+
+    data = read_query(
+        'SELECT username FROM professionals WHERE username = ?',
+        (nickname,)
+    )
+
+    return bool(data)
