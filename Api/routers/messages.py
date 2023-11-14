05d87b31a9493f74a5aa140cced2beccc170745f
stiliamm <redemption.records@icloud.com>
2023-11-14 16:32:53 +0200

    refactored messages

--- Api/routers/messages.py
+++ Api/routers/messages.py
@@ -1,9 +1,9 @@
 from services import messages_service
+from services.register_service import check_user_exist
 from fastapi import APIRouter, Header
 from data.responses import NotFound, Unauthorized
 from data.models.message import Message
-from common.auth import professional_or_401, company_or_401
-
+from common.auth import user_or_401
 
 messages_router = APIRouter(prefix='/messages')
 _ERROR_MESSAGE = 'You are not logged in!'
@@ -11,31 +11,29 @@ _ERROR_MESSAGE = 'You are not logged in!'
 #TODO Figure out a way to add audio recording ass message
 
 
-@messages_router.get('/{company_username}')
-def view_professional_messages(company_username: str, x_token: str = Header(default=None)):
-    prof = professional_or_401(x_token) if x_token else None
-    if prof:
-        return messages_service.get_messages(prof, company_username)
-    else:
-        return Unauthorized(content=_ERROR_MESSAGE)
-
-
-@messages_router.get('/{prof_username}')
-def view_company_messages(prof_username: str, x_token: str = Header(default=None)):
-    company = company_or_401(x_token) if x_token else None
-    if company:
-        return messages_service.get_messages(company, prof_username)
+@messages_router.get('/{receiver_username}')
+def view_user_messages(receiver_username: str, x_token: str = Header(default=None)):
+    user = user_or_401(x_token) if x_token else None
+    receiver = check_user_exist(receiver_username)
+    if user:
+        if receiver:
+            messages = messages_service.get_messages(user, receiver.username)
+            if not messages:
+                return NotFound(content=f'No messages with user: {receiver_username}')
+        else:
+            return NotFound(content=f'No user with username: {receiver_username}')    
     else:
         return Unauthorized(content=_ERROR_MESSAGE)
     
 
 @messages_router.post('/{receiver_username}')
 def send_message(receiver_username: str, message: Message, x_token: str = Header(default=None)):
-    prof = professional_or_401(x_token) if x_token else None
-    comp = company_or_401(x_token) if x_token else None
-    if prof:
-        return messages_service.create(prof, receiver_username, message)
-    elif comp:
-        return messages_service.create(comp, receiver_username, message)
+    user = user_or_401(x_token) if x_token else None
+    receiver = check_user_exist(receiver_username)
+    if user:
+        if receiver:
+            return messages_service.create(user, receiver.username, message)
+        else:
+            return NotFound(content=F'No user with username: {receiver_username}')
     else:
         return Unauthorized(content=_ERROR_MESSAGE)
