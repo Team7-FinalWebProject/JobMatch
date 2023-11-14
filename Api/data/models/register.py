51791d5a1275df90d85cc3e87283fc022dbbcf1d
stiliamm <redemption.records@icloud.com>
2023-11-14 19:22:36 +0200

    refactored to register user and prof/comp

--- Api/data/models/register.py
+++ Api/data/models/register.py
@@ -2,9 +2,18 @@ from pydantic import BaseModel, field_validator
 from common.constraints import Allowed_Username, Allowed_Register_Password
 
 
-class RegisterUserData(BaseModel):
+class RegisterProfessionalData(BaseModel):
     username: Allowed_Username
     password: Allowed_Register_Password = None
+    admin: str | None = None
+    first_name: staticmethod
+    last_name: str
+    address: str
+    user_id: str
+    summary: str
+    default_offer_id: int | None = None
+    picture: bytes | None = None
+    approved: bool | None = None
 
     @field_validator('password', mode='before')
     @classmethod
@@ -20,3 +29,51 @@ class RegisterUserData(BaseModel):
             return value
         raise ValueError(
             "Password must contain at least one lowercase letter, one uppercase letter, one digit, and one special character (@$!%*?&).")
+    
+
+class RegisterCompanyData(BaseModel):
+    username: Allowed_Username
+    password: Allowed_Register_Password = None
+    admin: bool | None = None
+    company_name: str
+    description: str
+    address: str
+    picture: bytes | None = None
+    approved: bool | None = None
+    user_id: int
+
+    @field_validator('password', mode='before')
+    @classmethod
+    def validate_password(cls, value):
+        '''Iterates over password and checks
+           if it contains all of the types of chars'''
+        if (
+            any(c.islower() for c in value) and
+            any(c.isupper() for c in value) and
+            any(c.isdigit() for c in value) and
+            any(c in "@$!%*?&" for c in value)
+        ):
+            return value
+        raise ValueError(
+            "Password must contain at least one lowercase letter, one uppercase letter, one digit, and one special character (@$!%*?&).")
+    
+
+
+
+
+
+
+#TODO see if better to be used out of the class
+
+# def validate_password(cls, value):
+#     '''Iterates over password and checks
+#         if it contains all of the types of chars'''
+#     if (
+#         any(c.islower() for c in value) and
+#         any(c.isupper() for c in value) and
+#         any(c.isdigit() for c in value) and
+#         any(c in "@$!%*?&" for c in value)
+#     ):
+#         return value
+#     raise ValueError(
+#         "Password must contain at least one lowercase letter, one uppercase letter, one digit, and one special character (@$!%*?&).")
