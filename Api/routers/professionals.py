0e9a8441a5a1bdc28df2d24176f2df139a773653
stiliamm <redemption.records@icloud.com>
2023-11-16 12:41:03 +0200

    added create offer and edit info

--- Api/routers/professionals.py
+++ Api/routers/professionals.py
@@ -1,28 +1,33 @@
 from fastapi import APIRouter, Header
-from common.auth import user_or_401
+from common.auth import professional_or_401
 from data.responses import BadRequest, Unauthorized
-from data.models.register import RegisterUserData
 from data.models.professional import Professional
-from data.models.company import Company
-from services import register_service
+from data.models.offer import ProfessionalOffer
+from services import professionals_service
 
 
 professionals_router = APIRouter(prefix='/professionals')
 
 
 @professionals_router.put('/info')
-def edit_info(x_token: str = Header(default=None)):
-    pass
+def edit_info(new_info: Professional, x_token: str = Header(default=None)):
+    prof = professional_or_401(x_token) if x_token else None
+    if not prof:
+        return Unauthorized(content='Invalid token')
+    return professionals_service.edit(new_info, prof)
+        
 
-
-@professionals_router.put('/offer')
-def set_prof_offer(x_token: str = Header(default=None)):
+@professionals_router.put('/default_offer')
+def set_default_prof_offer(x_token: str = Header(default=None)):
     pass
 
 
 @professionals_router.post('/offer')
-def create_offer(x_token: str = Header(default=None)):
-    pass
+def create_offer(new_offer: ProfessionalOffer, x_token: str = Header(default=None)):
+    prof = professional_or_401(x_token) if x_token else None
+    if not prof:
+        return Unauthorized(content='Invalid token')
+    return professionals_service.create_offer(new_offer, prof)
 
 
 @professionals_router.put('/edit/offer')
