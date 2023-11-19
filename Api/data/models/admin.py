ab08adc48dde5dd2b3b7af60c80d80cfac17c6c7
PetarSPal <petar.spalakarchev@yahoo.com>
2023-11-19 14:41:08 +0200

    feature: adin service wip

--- Api/data/models/admin.py
+++ Api/data/models/admin.py
@@ -11,3 +11,21 @@ class Admin(BaseModel):
         return cls(
             id=id,
             username=username)
+    
+class Config(BaseModel):
+    static_skills: bool | None = None
+    min_level: int | None = None
+    max_level: int | None = None
+    baseline_skills: dict | None = None
+    approved_skills: dict | None = None
+    pending_approval_skills: dict | None = None
+
+    @classmethod
+    def from_query_result(cls, static_skills, min_level, max_level, baseline_skills, approved_skills, pending_approval_skills):
+        return cls(
+            static_skills=static_skills,
+            min_level=min_level,
+            max_level=max_level,
+            baseline_skills=baseline_skills,
+            approved_skills=approved_skills,
+            pending_approval_skills=pending_approval_skills)
--- Api/services/admin_service.py
+++ Api/services/admin_service.py
@@ -1,7 +1,7 @@
 from services.register_service import _hash_password
 from secrets import compare_digest
-from data.database import read_query
-from data.models.admin import Admin
+from data.database import read_query, update_query
+from data.models.admin import Admin, Config
 from data.readers import reader_one
 
 def try_login_as_admin(username: str, password: str):
@@ -14,10 +14,58 @@ def find_admin_by_username(username: str):
     data = read_query(
         '''SELECT id, username, password from users
            WHERE username = %s''', (username,))
-    
     if data:
         password_bytes = bytes(data[0][-1])
         password_string = password_bytes.decode('utf-8')
         return password_string, reader_one(Admin, data)
     else:
         return None, None
+    
+def check_config():
+    data = read_query(
+        '''SELECT static_skills,min_level,max_level,baseline_skills,approved_skills,pending_approval_skills from config
+           WHERE lock = %s''', ('X',))
+    return reader_one(Config, data)
+
+def set_config(data: Config):
+    result = update_query(
+        '''UPDATE config
+        SET static_skills = COALESCE(%s, static_skills),
+        min_level = COALESCE(%s, min_level),
+        max_level = COALESCE(%s, max_level),
+        baseline_skills = COALESCE(%s, baseline_skills)
+        WHERE lock = %s''',
+        (data.static_skills, data.min_level, data.max_level, data.baseline_skills, 'X',))
+    ##TODO: check result and remodel
+    return result
+    
+def approve_pending_skills():
+    result = update_query(
+        '''UPDATE config
+        SET approved_skills = approved_skills || pending_approval_skills,
+        pending_approval_skills = ARRAY[]::text[];
+        WHERE lock = %s''', ('X',))
+    ##TODO: check result and remodel
+    return result
+    
+def discard_pending_skills():
+    result = update_query(
+        '''UPDATE config
+        SET pending_approval_skills = ARRAY[]::text[];
+        WHERE lock = %s''', ('X',))
+    ##TODO: check result and remodel
+    return result
+
+
+##TODO: Store in DB -> Baseline skills + extendible by admin + Extra skills for company + admin approval ?
+##Config table? Could also be a file
+####-------------------------------------------------------------------------------------------####
+_SKILL_NAMES = ['English', 'French', 'Computers']
+_SKILL_LEVELS = [0, 10]
+Allowed_Skill_Names = Annotated[str, lambda s: s in _SKILL_NAMES]
+Allowed_Skill_Levels = Annotated[int, lambda n: _SKILL_LEVELS[0] <= n <= _SKILL_LEVELS[1]]
+_USE_STATIC = False
+def switch_skills_mode():
+    _USE_STATIC = ~_USE_STATIC
+Skills = dict[str, tuple[int, str]] if not _USE_STATIC else dict[Allowed_Skill_Names, tuple[Allowed_Skill_Levels, str]]
+####-------------------------------------------------------------------------------------------####
