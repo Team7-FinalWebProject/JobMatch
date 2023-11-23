dbb7a9686dff2b072757c94a5a1f5e0454269beb
DonkoTonev <donkotonev04@gmail.com>
2023-11-23 18:11:49 +0200

    bug fix

--- Api/routers/admin.py
+++ Api/routers/admin.py
@@ -10,8 +10,8 @@ admin_router = APIRouter(prefix='/admin')
 ##TODO: Throw error instead of if user.__class__.__name__ else None etc ?
 @admin_router.get('/company/{id}', tags=["Admin"])
 def view_unapproved_company(id: int, x_token: str = Header()):
-    user = user_or_error(x_token)
-    return search_service.admin_get_unapproved_company_by_id(id) if user.__class__.__name__ == 'Admin' else None
+    admin = admin_or_error(x_token)
+    return search_service.admin_get_unapproved_company_by_id(id) if admin.__class__.__name__ == 'Admin' else None
 
 @admin_router.get('/companies', tags=["Admin"])
 def view_unapproved_companies(x_token: str = Header()):
