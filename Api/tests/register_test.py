5b8437abf363c17d9142276e7fcc91db8e6e02f1
stiliamm <redemption.records@icloud.com>
2023-11-24 14:51:17 +0200

    extended register tests

--- Api/tests/register_test.py
+++ Api/tests/register_test.py
@@ -32,7 +32,7 @@ invalid_comp_password =  {
 
 valid_professional_password = {
   "username": "X_6issk5EHKov7hri1Vn",
-  "password": "Par0l@@@@@",
+  "password": f"{valid_password}",
   "first_name": "string",
   "last_name": "string",
   "address": "string",
@@ -42,7 +42,7 @@ valid_professional_password = {
 
 valid_company_password = {
   "username": "dLvUmTeU",
-  "password": "Par0l@@@@@",
+  "password": f"{valid_password}",
   "company_name": "string",
   "description": "string",
   "address": "string",
@@ -62,9 +62,20 @@ def test_register_company_invalid_password_422():
 def test_register_professional_valid_200():
     response = client.post("/register/professionals", json=valid_professional_password)
     assert response.status_code == 200
+    response = response.json()
+    assert "username" in response
+    assert "first_name" in response
+    assert "last_name" in response
+    assert "summary" in response
+
 
 def test_register_company_valid_200():
     response = client.post("/register/companies", json=valid_company_password)
     assert response.status_code == 200
+    response = response.json()
+    assert "username" in response
+    assert "company_name" in response
+    assert "description" in response
+    assert "address" in response
 
     
