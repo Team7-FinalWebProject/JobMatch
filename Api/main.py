8c4ecbf676f0d6555513dd49684909beca4ef93b
PetarSPal <petar.spalakarchev@yahoo.com>
2023-11-25 14:02:39 +0200

    Fix: CORS should point to FE, not BE port

--- Api/main.py
+++ Api/main.py
@@ -16,12 +16,12 @@ from routers.messages import messages_router
 
 app = FastAPI()
 origins = [
-    "http://localhost:8000",
-    "localhost:8000",
-    "https://localhost:8000",
-    "127.0.0.1:8000",
-    "http://127.0.0.1:8000",
-    "https://127.0.0.1:8000",
+    "http://localhost:3000",
+    "localhost:3000",
+    "https://localhost:3000",
+    "127.0.0.1:3000",
+    "http://127.0.0.1:3000",
+    "https://127.0.0.1:3000",
 ]
 
 
