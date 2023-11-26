e6686c64207341a61f586080017898385111084d
PetarSPal <petar.spalakarchev@yahoo.com>
2023-11-26 20:22:09 +0200

    fix: display numbers

--- Jobclient/src/pages/displayData.tsx
+++ Jobclient/src/pages/displayData.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 type Data = {
-  [key: string]: string | Data;
+  [key: string]: string | number | Data | null;
 };
 
 const DataDisplay: React.FC<{ data: Data | null }> = ({ data }) => {
@@ -26,10 +26,14 @@ const DataDisplay: React.FC<{ data: Data | null }> = ({ data }) => {
 };
 
 const renderValue = (value: string | Data): React.ReactNode => {
-  if (typeof value === 'string') {
+  if (typeof value === 'string' || typeof value == 'number') {
     return value;
   } else if (typeof value === 'object' && value !== null) {
     return <DataDisplay data={value} />;
+  // } else if (typeof value === 'object' && !value) {
+  //   return <><DataDisplay data={value} /><li>{'>empty<'}</li></>;
+  } else if (value === null) {
+    return ">null<"
   } else {
     return null;
   }
