069f3b228a74b09ab26dbc62fd3ae08f05b2dcdc
DonkoTonev <donkotonev04@gmail.com>
2023-11-27 16:53:34 +0200

    Added more tests for the admin func
    The tests are not runned, but will do it.

--- Api/tests/admin_test.py
+++ Api/tests/admin_test.py
@@ -14,6 +14,11 @@ change_config_valid_info = {
   "max_level": 10
 }
 
+discard_prepared_valid_info = [
+  "Skill1",
+  "Skill2"
+]
+
 
 
 def test_view_unapproved_company_valid_data_200(admintoken):
@@ -89,4 +94,65 @@ def test_change_config_valid_data_200(admintoken):
 def test_change_config_no_token_401(admintoken):
     response = client.patch("/admin/config", json=change_config_valid_info, headers={})
     assert response.status_code == 401
-    
+
+
+def test_prepare_skills_valid_data_200(admintoken):
+    response = client.post("/admin/prepare_skills", headers={"X-Token": admintoken})
+    assert response.status_code == 200
+
+
+def test_change_config_no_token_401(admintoken):
+    response = client.post("/admin/prepare_skills", headers={})
+    assert response.status_code == 401
+
+
+def test_delete_pending_valid_data_200(admintoken):
+    response = client.delete("/admin/delete_pending", headers={"X-Token": admintoken})
+    assert response.status_code == 200
+
+
+def test_delete_pending_no_token_401(admintoken):
+    response = client.delete("/admin/delete_pending", headers={})
+    assert response.status_code == 401
+
+
+def test_discard_prepared_valid_data_200(admintoken):
+    response = client.delete("/admin/discard_prepared", json=discard_prepared_valid_info, headers={"X-Token": admintoken})
+    assert response.status_code == 200
+
+
+def test_discard_prepared_no_token_401(admintoken):
+    response = client.delete("/admin/discard_prepared", json=discard_prepared_valid_info, headers={})
+    assert response.status_code == 401
+
+
+def test_commit_skills_valid_data_200(admintoken):
+    response = client.post("/admin/commit_skills", headers={"X-Token": admintoken})
+    assert response.status_code == 200
+
+
+def test_commit_skills_no_token_401(admintoken):
+    response = client.post("/admin/commit_skills", headers={})
+    assert response.status_code == 401
+
+
+def test_approve_professional_valid_data_200(admintoken):
+    response = client.post("/admin/approve_professional?prof_id=1", headers={"X-Token": admintoken})
+    assert response.status_code == 200
+
+
+def test_approve_professional_no_token_401(admintoken):
+    response = client.post("/admin/approve_professional?prof_id=1", headers={})
+    assert response.status_code == 401
+
+
+
+def test_approve_company_valid_data_200(admintoken):
+    response = client.post("/admin/approve_company?comp_id=1", headers={"X-Token": admintoken})
+    assert response.status_code == 200
+
+
+def test_approve_company_no_token_401(admintoken):
+    response = client.post("/admin/approve_company?comp_id=1", headers={})
+    assert response.status_code == 401
+
