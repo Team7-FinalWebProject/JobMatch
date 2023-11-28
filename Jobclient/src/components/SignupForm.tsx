f23416fdf7eb6ca7a730bba9eb38a0359b14a5cf
stiliamm <redemption.records@icloud.com>
2023-11-28 15:55:02 +0200

    refactored design for signup

--- Jobclient/src/components/SignupForm.tsx
+++ Jobclient/src/components/SignupForm.tsx
@@ -1,5 +1,7 @@
+import React from "react"
 
 export default function SignupForm() {
+
     return (
       <>
         {/*
@@ -18,22 +20,21 @@ export default function SignupForm() {
               alt="Your Company"
             />
             <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-              Sign in to your account
+              Sign Up As Professional
             </h2>
           </div>
   
           <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
             <form className="space-y-6" action="#" method="POST">
               <div>
-                <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
-                  Email address
+                <label htmlFor="username" className="block text-sm font-medium leading-6 text-gray-900">
+                  Username
                 </label>
                 <div className="mt-2">
                   <input
-                    id="email"
-                    name="email"
-                    type="email"
-                    autoComplete="email"
+                    id="username"
+                    name="username"
+                    type="username"
                     required
                     className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                   />
@@ -45,11 +46,6 @@ export default function SignupForm() {
                   <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
                     Password
                   </label>
-                  <div className="text-sm">
-                    <a href="#" className="font-semibold text-indigo-600 hover:text-indigo-500">
-                      Forgot password?
-                    </a>
-                  </div>
                 </div>
                 <div className="mt-2">
                   <input
@@ -62,23 +58,107 @@ export default function SignupForm() {
                   />
                 </div>
               </div>
+
+              <div>
+                <div className="flex items-center justify-between">
+                  <label htmlFor="firstname" className="block text-sm font-medium leading-6 text-gray-900">
+                    First Name
+                  </label>
+                </div>
+                <div className="mt-2">
+                  <input
+                    id="firstname"
+                    name="firstname"
+                    type="firstname"
+                    required
+                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  />
+                </div>
+              </div>
+
+              <div>
+                <div className="flex items-center justify-between">
+                  <label htmlFor="lastname" className="block text-sm font-medium leading-6 text-gray-900">
+                    Last Name
+                  </label>
+                </div>
+                <div className="mt-2">
+                  <input
+                    id="lastname"
+                    name="lastname"
+                    type="lastname"
+                    required
+                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  />
+                </div>
+              </div>
+
+              <div>
+                <div className="flex items-center justify-between">
+                  <label htmlFor="address" className="block text-sm font-medium leading-6 text-gray-900">
+                    Address
+                  </label>
+                </div>
+                <div className="mt-2">
+                  <input
+                    id="address"
+                    name="address"
+                    type="address"
+                    required
+                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  />
+                </div>
+              </div>
+
+              <div>
+                <div className="flex items-center justify-between">
+                  <label htmlFor="summary" className="block text-sm font-medium leading-6 text-gray-900">
+                    Summary
+                  </label>
+                </div>
+                <div className="mt-2">
+                  <input
+                    id="summary"
+                    name="summary"
+                    type="summary"
+                    required
+                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  />
+                </div>
+              </div>
+
+              <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+                <label className="block text-sm font-medium text-gray-700">
+                    Upload Photo
+                </label>
+                <div className="mt-1 flex items-center">
+                    <input
+                    type="file"
+                    accept="image/*"
+                    // onChange={handlePhotoChange}
+                    className="hidden"
+                    />
+                    <label
+                    htmlFor="upload-photo"
+                    className="cursor-pointer text-sm text-blue-600 hover:underline ml-2 justify-right"
+                    >
+                    Choose a file
+                    </label>
+                </div>
+                </div>
+            
+
   
               <div>
                 <button
                   type="submit"
                   className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                 >
-                  Sign in
+                  Create account
                 </button>
               </div>
             </form>
   
-            <p className="mt-10 text-center text-sm text-gray-500">
-              Not a member?{' '}
-              <a href="#" className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">
-                Start a 14 day free trial
-              </a>
-            </p>
           </div>
         </div>
       </>
