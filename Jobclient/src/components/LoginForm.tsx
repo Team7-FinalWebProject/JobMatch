24fe3417e84fda451fc135dd02ed94c0cd8b1295
stiliamm <redemption.records@icloud.com>
2023-11-29 16:36:55 +0200

    design changes

--- Jobclient/src/components/LoginForm.tsx
+++ Jobclient/src/components/LoginForm.tsx
@@ -1,4 +1,5 @@
 import '../index.css';
+import jobutopia_logo from '../assets/images/jobutopia-logo-black.png'
 interface LoginForm_Props {
   onSubmit: (username: string, password: string) => void;
 }
@@ -27,11 +28,11 @@ const LoginForm: React.FC<LoginForm_Props> = ({ onSubmit }) => {
         */}
         <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
           <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-            {/* <img
-              className="mx-auto h-10 w-auto"
-              src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+            <img
+              className="mx-auto h-15 w-auto"
+              src={ jobutopia_logo }
               alt="Your Company"
-            /> */}
+            />
             <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
               Sign in to your account
             </h2>
