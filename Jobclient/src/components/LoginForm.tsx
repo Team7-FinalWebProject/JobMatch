c5ad9c0f3e0b8ba5b10e7df1599e2d8ff2f0604a
stiliamm <redemption.records@icloud.com>
2023-11-29 21:11:55 +0200

    tested adding svg background

--- Jobclient/src/components/LoginForm.tsx
+++ Jobclient/src/components/LoginForm.tsx
@@ -1,5 +1,6 @@
 import '../index.css';
 import jobutopia_logo from '../assets/images/jobutopia-logo-black.png'
+import backgroundSVG from '../assets/endless-constellation.svg'
 interface LoginForm_Props {
   onSubmit: (username: string, password: string) => void;
 }
@@ -26,14 +27,14 @@ const LoginForm: React.FC<LoginForm_Props> = ({ onSubmit }) => {
           <body class="h-full">
           ```
         */}
-        <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+        <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8" style={{ backgroundImage: `url(${backgroundSVG})` }}>
           <div className="sm:mx-auto sm:w-full sm:max-w-sm">
             <img
               className="mx-auto h-15 w-auto"
               src={ jobutopia_logo }
               alt="Your Company"
             />
-            <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+            <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-white">
               Sign in to your account
             </h2>
           </div>
