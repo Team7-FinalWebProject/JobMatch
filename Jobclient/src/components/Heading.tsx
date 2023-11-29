70ea936252dad1f911880aa3eeddcaf2de6cdf3b
stiliamm <redemption.records@icloud.com>
2023-11-29 18:42:40 +0200

    fixed header to top: rest of page moves under

--- Jobclient/src/components/Heading.tsx
+++ Jobclient/src/components/Heading.tsx
@@ -27,7 +27,7 @@ export default function Example() {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
 
   return (
-    <header className="bg-white border-b border-black">
+    <header className="bg-gray-200 border-b border-black fixed top-0 left-0 w-full z-50">
       <nav className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8 mt-2" aria-label="Global">
         <div className="flex lg:flex-1">
           <a href="#" className="-m-1.5 p-1.5">
