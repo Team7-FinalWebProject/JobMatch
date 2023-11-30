1eb664cec46fd7058f32450b930c90de8b9c81fd
DonkoTonev <donkotonev04@gmail.com>
2023-11-30 21:14:11 +0200

    Just added a few tests. Will continue

--- Api/tests/company_test.py
+++ Api/tests/company_test.py
@@ -17,12 +17,19 @@ valid_comp_info = {
 
 
 
-def test_edit_company_info_valid_info_200(proftoken):
-    response = client.put("/companies/info", json=valid_comp_info, headers={"X-Token": proftoken})
+def test_edit_company_info_valid_info_200(companytoken):
+    response = client.put("/companies/info", json=valid_comp_info, headers={"X-Token": companytoken})
     assert response.status_code == 200
     assert response.json()["name"] == valid_comp_info["name"]
     assert response.json()["description"] == valid_comp_info["description"]
     assert response.json()["address"] == valid_comp_info["address"]
     assert response.json()["picture"] == valid_comp_info["picture"]
+
+
+
+def test_edit_company_info__no_token_401(companytoken):
+    response = client.put("/companies/info", json=valid_comp_info)
+    assert response.status_code == 401
+
 
 
