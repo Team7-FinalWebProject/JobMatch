6943788dfde6224661a63e78a1e8dd42875b7bbc
DonkoTonev <donkotonev04@gmail.com>
2023-12-01 21:36:20 +0200

    It fetches and shows the profs It needs more styling

--- Jobclient/src/pages/Professional.tsx
+++ Jobclient/src/pages/Professional.tsx
@@ -1,21 +1,19 @@
-// UserList.js
-
 import Cookies from "universal-cookie";
-import { submitMessage } from "../services/submitMessage";
-import MessagesForm from "../components/MessageForm";
-
 // UserList.js
 import React, { useEffect, useState } from 'react';
-import axios from 'axios';
+import { getApprovedProfessionals } from  "../services/getAllProfessionals.js";
 
 function UserList() {
   const [professionals, setProfessionals] = useState([]);
+  const cookies = new Cookies();
+  const getAuthToken = () => cookies.get('authToken');
+  let authToken = getAuthToken();
 
   useEffect(() => {
     async function fetchProfessionals() {
       try {
-        const response = await axios.get('http://localhost:8000/search/professionals');
-        setProfessionals(response.data);
+        const data = await getApprovedProfessionals(authToken);
+        setProfessionals(data);
       } catch (error) {
         console.error('Error fetching professionals', error);
       }
@@ -25,13 +23,25 @@ function UserList() {
   }, []);
 
   return (
-    <div>
+    <div style={{ padding: '20px' }}>
       <h2>Professional List</h2>
-      <ul>
+      <div style={{ display: 'flex', flexWrap: 'wrap' }}>
         {professionals.map((professional) => (
-          <li key={professional.id}>{professional.name}</li>
+          <div
+            key={professional.id}
+            style={{
+              border: '1px solid #ccc',
+              padding: '10px',
+              margin: '10px',
+              width: '200px',
+              textAlign: 'center',
+            }}
+          >
+            <p>{professional.first_name} {professional.last_name}</p>
+            <p>Status: {professional.status}</p>
+          </div>
         ))}
-      </ul>
+      </div>
     </div>
   );
 }
