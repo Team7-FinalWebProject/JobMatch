739fc37cf3f98d6a49df9b0de65f5e4321e2f3ff
stiliamm <redemption.records@icloud.com>
2023-12-06 23:02:06 +0200

    finished offers page

--- Jobclient/src/services/getAvatar.js
+++ Jobclient/src/services/getAvatar.js
@@ -1,17 +1,17 @@
 export const getAvatar = async (authToken, apiUrl) => {
 
     try {
-        const response = await fetch(apiUrl, {
-        method: 'GET',
-        headers: { 'x-token': authToken },
+        const response = await axios.get(apiUrl, {
+            method: 'GET',
+            headers: { 'x-token': authToken },
+            responseType: 'blob',
         });
 
         if (!response.ok) {
         throw new Error('Failed to fetch photo');
         }
 
-        const data = await response.blob();
-        return data;
+        return response.data;
     } catch (error) {
         console.error('Error fetching photo:', error.message);
         return null;
