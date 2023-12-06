d460adc23b6c4ef1c05ec9e215ef0009ce58ecbe
stiliamm <redemption.records@icloud.com>
2023-12-06 14:01:52 +0200

    added to group layout for rendering

--- Jobclient/src/pages/Home.tsx
+++ Jobclient/src/pages/Home.tsx
@@ -1,19 +1,13 @@
-import TopNav from "./TopNav";
-import LeftNav from "./LeftNav";
+import Layout from "./Layout";
 import Content from "./Content";
 
-function Home() {
-  return(
-    <div className="flex">
-    <LeftNav />
-    <div className="flex-1">
-      <TopNav />
-      <div className="mt-4">
-        <Content />
-      </div>
-    </div>
-  </div>
-  )
-}
 
-export default Home
+const Home: React.FC = () => {
+  return (
+    <Layout>
+      <Content />
+    </Layout>
+  );
+};
+
+export default Home;
